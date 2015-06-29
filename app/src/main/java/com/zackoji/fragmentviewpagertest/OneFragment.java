2ba186ba1b8b0058cb4c862b701087f89fd241b1
shredktp <kornth_ebasicxp@hotmail.com>
2015-06-29 11:21:32 +0700

    Initial

---
+++ app/src/main/java/com/zackoji/fragmentviewpagertest/OneFragment.java
@@ -0,0 +1,23 @@
+package com.zackoji.fragmentviewpagertest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+/**
+ * Created by Zackoji on 29/6/2558.
+ */
+public class OneFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_one, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/zackoji/fragmentviewpagertest/ThreeFragment.java
@@ -0,0 +1,21 @@
+package com.zackoji.fragmentviewpagertest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Zackoji on 29/6/2558.
+ */
+public class ThreeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_three, container, false);
+        return rootView;
+    }
+}
